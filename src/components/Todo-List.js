6d0e430d59d074b9c3cb741e0d680ee4ee7c51ab
jakewu0222 <wuyuyu0222@gmail.com>
2019-01-04 16:45:36 +0800

    restructure & lazy loading

--- src/components/Todo-List.js
+++ src/components/Todo-List.js
@@ -17,49 +17,47 @@ export default class TodoList extends Component {
 
 
   render() {
-    return (
-      <Grid container spacing={24}>
-        <List list={this.props.list} />
-      </Grid>
-    )
-  }
-}
-
-const List = (props) => {
-  if (props.list.length > 0) {
-    const list = props.list.map(item => (
-      <Grid item xs={12} key={item.id}>
-        <Paper>
-          <div className="todo-block">
-            <div className="todo-title">{item.title}</div>
-            <div className="todo-progress">
-              {(item.progress > 0 && item.progress < 100) ? item.progress + '%' : item.status}
-              <LinearProgress classes={{
-                colorPrimary: 'primary-progress-color',
-                barColorPrimary: 'primary-progress-bar-color'
-              }}
-                variant="determinate" color="primary" value={item.progress} />
-            </div>
-            <div className="todo-content">{item.content}</div>
-            <div className="todo-author">{item.userId} at {item.modifiedAt.toLocaleDateString()}</div>
-            <div className="todo-action">
-              <Button>Edit</Button>
-              <Button color="secondary">Delete</Button>
-            </div>
+    const { list } = this.props;
+    if (list.length > 0) {
+      return (
+        <Grid container spacing={24}>
+          {list.map(item => (
+            <Grid item xs={12} key={item.id}>
+              <Paper>
+                <div className="todo-block">
+                  <div className="todo-title">{item.title}</div>
+                  <div className="todo-progress">
+                    {(item.progress > 0 && item.progress < 100) ? item.progress + '%' : item.status}
+                    <LinearProgress classes={{
+                      colorPrimary: 'primary-progress-color',
+                      barColorPrimary: 'primary-progress-bar-color'
+                    }}
+                      variant="determinate" color="primary" value={item.progress} />
+                  </div>
+                  <div className="todo-content">{item.content}</div>
+                  <div className="todo-author">{item.userId} at {item.modifiedAt.toLocaleDateString()}</div>
+                  <div className="todo-action">
+                    <Button>Edit</Button>
+                    <Button color="secondary">Delete</Button>
+                  </div>
+                </div>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      )
+    } else {
+      return (
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Paper>
+              <div className="todo-block">
+                create new todo
           </div>
-        </Paper>
-      </Grid>
-    ))
-    return list;
-  } else {
-    return (
-      <Grid item xs={12}>
-        <Paper>
-          <div className="todo-block">
-            create new todo
-        </div>
-        </Paper>
-      </Grid>
-    )
+            </Paper>
+          </Grid>
+        </Grid>
+      )
+    }
   }
 }
